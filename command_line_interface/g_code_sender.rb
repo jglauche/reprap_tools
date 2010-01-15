19eb54224710de81465e96be1a99d9bde1cc2e68
jglauche <jglauche@celestra.(none)>
2010-01-15 22:16:56 +0100

    work in progress

--- command_line_interface/g_code_sender.rb
+++ command_line_interface/g_code_sender.rb
@@ -1,19 +1,89 @@
 class GCodeSender
   
+  attr_accessor :gcode
+  
   def initialize(port)
     @port = port
-    check_port
+    @line = 0
+    @lines = 0
+    @current = 0
+    @gcode = nil
+    open_port
   end
   
-  def check_port
+  def open_port
     return false unless @port
     begin
-      f = File.open(@port,"r+")
-      f.close
+      @pipe = File.open(@port,"r+")
+    
       return true
     rescue  
+      puts "Cannot connect to #{@port}"
+      exit
       return false
     end
   end
+  
+  def send_cmd(cmd)
+    puts "will send command #{cmd}"
+    begin
+      @pipe.puts cmd
+      response = []
+      while(response.size == 0) do
+        sleep 0.1
+        response = @pipe.readlines
+      end
+      got_ok = false
+      response.each do |res|
+        if res.include? "Resend"
+          @line = res.gsub("\r\n","").gsub("Resend:","").to_i
+          puts "comm error, have to resend #{@line}"
+          return false
+        end  
+        if res.include? "T:"
+          puts res # qnd   
+        end
+        if res.include? "ok"
+          got_ok = true
+          puts "ok"
+        end
+      end
+      return got_ok      
+    rescue Exception => ex
+      puts "Exception #{ex} at send_cmd"
+      return nil
+    end
+  end
+
+  def send_line(number)
+    cmd = gcode[number].strip
+    cmd += " N#{number}"
+    checksum = cmd.bytes.inject{|a,b| a^b}
+    res = send_cmd(cmd+"*"+checksum.to_s)
+    unless res == false
+      @line += 1
+    end
+  end
+  
+  def start
+    send_cmd("M110 -1") # reset line count
+    @line = 0
+    
+    if @gcode == nil
+      puts "nothing to send"
+      return
+    end
+    
+    @lines = gcode.size
+   
+    puts "sending #{@lines} lines to the machine"
+    while @lines > @line 
+      send_line(@line)
+    end
+    puts "done."
+        
+        
+  end
+  
 
 end
--- command_line_interface/print.rb
+++ command_line_interface/print.rb
@@ -1,9 +1,14 @@
 #!/usr/bin/env ruby
 
+require "rubygems"
+require "activesupport"
+
 require 'g_code_sender'
 
 # todo: Probe USB ports
-sender = GCodeSender.new("/dev/ttyUSB0")
+interface = "/dev/ttyUSB2"
+sender = GCodeSender.new(interface)
+
 
 dryrun = true
 
@@ -14,8 +19,12 @@ f.close
 # stage 1: fix g-code/multiply
 # TODO
 
+sender.gcode = contents
+
 # stage 2: heat up and stuff
+# TODO
 
 # stage 3: print
 
+sender.start
 
